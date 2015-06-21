e3f8e2ede79885812c5c15b97dafe2b40ce6a9dd
Alexandre Stuber <alestuber@gmail.com>
2015-06-21 18:59:34 -0300

    Add password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
+  validates :password, length: { minimum: 8 }, allow_blank: true
   has_secure_password
 end
